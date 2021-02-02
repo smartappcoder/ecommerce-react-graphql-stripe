cbb8492cac477913de9bfdab9dc365d4f7ac2ed2
Smart App Crafter <dennis.sheu@gmail.com>
2021-02-02 00:12:12 -0800

    Update strapi controller codes.

--- order-controller.js
+++ order-controller.js
@@ -1,22 +1,12 @@
-const stripe = require('stripe')('stripe-secret-key');
+'use strict';
 
-/* 
- Call from React client
-      await strapi.createEntry("orders", {
-        amount,
-        brews: cartItems,
-        city,
-        postalCode,
-        address,
-        token
-      });
-*/
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const stripe = require('stripe')('sk_test_51IFCN9HTVOK6Lsc3s0ghPqfLq35psqNUH3VwAI12QVwKf8QS4vIze1utVfbBZ1uYwvQoea2YJL7KPnxsc6ZL9PHK00PhtVIKpP');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
 
 module.exports = {
     /**
@@ -44,7 +34,7 @@ module.exports = {
     console.log("Address    : ", address);
     console.log("Token      : ", token);
 
-    // // send charge to Stripe (old code from class does not work
+    // // send charge to Stripe
     // const charges = await stripe.charges.create({
     //     amount: amount * 100,
     //     currency: 'USD',
@@ -52,7 +42,6 @@ module.exports = {
     //     source: token
     // });
 
-    // Update from stripe doc.
     // `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
     const charges = await stripe.charges.create({
       amount: amount * 100,
@@ -65,16 +54,25 @@ module.exports = {
     console.log("ctx.state.user    : ", ctx.state.user);
 
     // Create order to database
-    const order = await strapi.services.orders.add({
+    // const order = await strapi.services.orders.add({
+    //     user: ctx.state.user._id,
+    //     address,
+    //     amount,
+    //     brews,
+    //     postalCode,
+    //     city
+    // });
+    // console.log("Order       : ", order);
+
+    let entity;
+    entity = await strapi.services.orders.create({
         user: ctx.state.user._id,
         address,
         amount,
         brews,
         postalCode,
-        city
+        city      
     });
-    console.log("Order       : ", order);
-
-    return order;
+    return sanitizeEntity(entity, { model: strapi.models.orders });
   },
 };
